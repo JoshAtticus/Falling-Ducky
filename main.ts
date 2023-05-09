e5fb67b28a78f3c5791a91e435d8e3081f15706a
Josh <72828296+JoshAtticus@users.noreply.github.com>
2023-05-09 10:58:07 +0800

    FINE

--- main.ts
+++ main.ts
@@ -20,15 +20,6 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Gap, function (sprite, otherSpri
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
     info.changeLifeBy(-1)
 })
-controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (game.ask("Toggle BGM (A)", "Cancel (B)")) {
-        if (bgm == 1) {
-            bgm = 0
-        } else {
-            bgm = 1
-        }
-    }
-})
 info.onLifeZero(function () {
     game.over(false)
 })
@@ -38,7 +29,6 @@ let gapImage: Image = null
 let bottomImage: Image = null
 let topImage: Image = null
 let gap = 0
-let bgm = 0
 let mySprite: Sprite = null
 info.setLife(3)
 game.setGameOverMessage(false, "Out of lives :(")
@@ -675,11 +665,4 @@ game.onUpdateInterval(1500, function () {
     projectile.top = 0
     projectile = sprites.createProjectileFromSide(bottomImage, -45, 0)
     projectile.bottom = scene.screenHeight()
-})
-forever(function () {
-    if (bgm == 1) {
-        music.play(music.stringPlayable("F G A F - F G A ", 300), music.PlaybackMode.UntilDone)
-        music.play(music.stringPlayable("E D E E F G A E ", 300), music.PlaybackMode.UntilDone)
-        music.play(music.stringPlayable("E F G D E F D E ", 300), music.PlaybackMode.UntilDone)
-    }
 })
